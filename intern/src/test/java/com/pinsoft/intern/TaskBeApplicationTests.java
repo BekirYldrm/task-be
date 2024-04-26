ccae112084203f4d964e689e71429ce52388b0c3
Bekir <bekiryildirim.2508@gmail.com>
2024-04-26 12:39:15 +0300

    first commit

---
+++ intern/src/test/java/com/pinsoft/intern/TaskBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pinsoft.intern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

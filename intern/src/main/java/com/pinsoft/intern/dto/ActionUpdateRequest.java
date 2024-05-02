83f08d80ee585684fe7ccb1c5d3d0355af3d83fa
Bekir <bekiryildirim.2508@gmail.com>
2024-05-02 16:09:00 +0300

    dto for patch requests

---
+++ intern/src/main/java/com/pinsoft/intern/dto/ActionUpdateRequest.java
@@ -0,0 +1,17 @@
+package com.pinsoft.intern.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ActionUpdateRequest {
+    private String description;
+    private float money;
+    private String type;
+    private Long id;
+}
